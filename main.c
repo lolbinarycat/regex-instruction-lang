5a7b22530987a1c0360cb63d637266a094a7f163
lolbinarycat <dogedoge61+github@gmail.com>
2016-07-07 12:53:49 -0600

    Create main.c
    added framework

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main( int argc, const char* argv[] ) {
+	
+	switch ( argv[1] ) {
+	case "r": //replace command
+	//code
+	case "t": //test command
+	//code
+	default:
+	//code
+	}
+
+}
